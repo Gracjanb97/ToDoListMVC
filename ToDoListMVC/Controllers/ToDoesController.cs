daae854673d9a5f706f60c2236fc1b6833a03844
Dawidson957 <45687438+Dawidson957@users.noreply.github.com>
2020-06-23 17:42:50 +0200

    Added ToDo Controller with basic crud operations

---
+++ ToDoListMVC/Controllers/ToDoesController.cs
@@ -0,0 +1,137 @@
+﻿using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ToDoListMVC.Models;
+
+namespace ToDoListMVC.Controllers
+{
+    public class ToDoesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ToDoesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        // GET: ToDoes
+        public ActionResult Index()
+        {
+            string currentUserId = User.Identity.GetUserId();
+            var currentUser = _context.Users.FirstOrDefault(x => x.Id == currentUserId);
+            var toDosList = _context.ToDos.ToList().Where(x => x.User == currentUser);
+
+            return View(toDosList);
+        }
+
+        // GET: ToDoes/Details/{id}
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ToDo toDo = _context.ToDos.Find(id);
+            if (toDo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(toDo);
+        }
+
+        // GET: ToDoes/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ToDoes/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Description,IsDone")] ToDo toDo)
+        {
+            if (ModelState.IsValid)
+            {
+                string currentUserId = User.Identity.GetUserId();
+                var currentUser = _context.Users.FirstOrDefault(x => x.Id == currentUserId);
+                toDo.User = currentUser;
+
+                _context.ToDos.Add(toDo);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(toDo);
+        }
+
+        // GET: ToDoes/Edit/{id}
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ToDo toDo = _context.ToDos.Find(id);
+            if (toDo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(toDo);
+        }
+
+        // POST: ToDoes/Edit/{id}
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Description,IsDone")] ToDo toDo)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Entry(toDo).State = EntityState.Modified;
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(toDo);
+        }
+
+        // GET: ToDoes/Delete/{id}
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ToDo toDo = _context.ToDos.Find(id);
+            if (toDo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(toDo);
+        }
+
+        // POST: ToDoes/Delete/{id}
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            ToDo toDo = _context.ToDos.Find(id);
+            _context.ToDos.Remove(toDo);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                _context.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
